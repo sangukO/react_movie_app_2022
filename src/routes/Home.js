c9fd8c8117b3b6127b3b428b08618ceba2362bff
sangukO <woqls1972@gmail.com>
2022-08-11 21:41:58 +0900

    한영변환수정중

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@ import React, {Fragment, useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import Movie from "../components/Movie";
 import People from "../components/People";
-import { Input, Dropdown, Menu, Space } from "antd";
+import { Input, Dropdown, Menu, Space, Button } from "antd";
 import { DownOutlined } from '@ant-design/icons';
 const { Search } = Input;
 
@@ -15,30 +15,100 @@ function Home() {
   };
   const [searchKey, setSearchKey] = useState("");
   const [boolean, setBoolean] = useState(false);
+  const [lang, setLang] = useState();
   const onSearch = async() => {
+    setResult(query);
     if(query == "" || query == " ") {
       return null;
     }
-    if(searchKey == "movie") {
-      const json = await (
-        await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=522ad530f050aff49180c9834add195d&language=ko&include_adult=false&query=${query}`
-        )
-      ).json();
-      setMovies(json.results);
-      setBoolean(false);
-      setResult(query);
-    }
-    if (searchKey == "people") {
-      const json = await (
-        await fetch(
-        `
-        https://api.themoviedb.org/3/search/person?api_key=522ad530f050aff49180c9834add195d&language=ko-KR&query=${query}&page=1&include_adult=false`
-        )
-      ).json();
-      setMovies(json.results);
-      setBoolean(true);
-      setResult(query);
+    if(result) {
+      if(lang){
+        if(searchKey == "movie") {
+          const json = await (
+            await fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=522ad530f050aff49180c9834add195d&language=ko-KR&include_adult=false&query=${result}&include_adult=false`
+            )
+          ).json();
+          setMovies(json.results);
+          setBoolean(false);
+          setResult(query);
+        }
+        if (searchKey == "people") {
+          const json = await (
+            await fetch(
+            `https://api.themoviedb.org/3/search/person?api_key=522ad530f050aff49180c9834add195d&language=ko-KR&query=${result}&page=1&include_adult=false`
+            )
+          ).json();
+          setMovies(json.results);
+          setBoolean(true);
+          setResult(query);
+        }
+      } else {
+        if(searchKey == "movie") {
+          const json = await (
+            await fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=522ad530f050aff49180c9834add195d&language=en-EN&include_adult=false&query=${result}&include_adult=false`
+            )
+          ).json();
+          setMovies(json.results);
+          setBoolean(false);
+          setResult(query);
+        }
+        if (searchKey == "people") {
+          const json = await (
+            await fetch(
+            `https://api.themoviedb.org/3/search/person?api_key=522ad530f050aff49180c9834add195d&language=en-EN&query=${result}&page=1&include_adult=false`
+            )
+          ).json();
+          setMovies(json.results);
+          setBoolean(true);
+          setResult(query);
+        }
+      }
+    } else {
+      if (lang) {
+        if(searchKey == "movie") {
+          const json = await (
+            await fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=522ad530f050aff49180c9834add195d&language=ko-KR&include_adult=false&query=${query}&include_adult=false`
+            )
+          ).json();
+          setMovies(json.results);
+          setBoolean(false);
+          setResult(query);
+        }
+        if (searchKey == "people") {
+          const json = await (
+            await fetch(
+            `https://api.themoviedb.org/3/search/person?api_key=522ad530f050aff49180c9834add195d&language=ko-KR&query=${query}&page=1&include_adult=false`
+            )
+          ).json();
+          setMovies(json.results);
+          setBoolean(true);
+          setResult(query);
+        }
+      } else {
+        if(searchKey == "movie") {
+          const json = await (
+            await fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=522ad530f050aff49180c9834add195d&language=en-EN&include_adult=false&query=${query}&include_adult=false`
+            )
+          ).json();
+          setMovies(json.results);
+          setBoolean(false);
+          setResult(query);
+        }
+        if (searchKey == "people") {
+          const json = await (
+            await fetch(
+            `https://api.themoviedb.org/3/search/person?api_key=522ad530f050aff49180c9834add195d&language=en-EN&query=${query}&page=1&include_adult=false`
+            )
+          ).json();
+          setMovies(json.results);
+          setBoolean(true);
+          setResult(query);
+        }
+      }
     }
     v = "2";
     };
@@ -46,16 +116,40 @@ function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovie = async() => {
-    const json = await (
-      await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=522ad530f050aff49180c9834add195d&language=ko-KR&page=1`
-      )
-    ).json();
-    setMovies(json.results);
-    setLoading(false);
+    if (result) {
+      onSearch();
+    }
+    else {
+      if(!lang) {
+        const json = await (
+          await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=522ad530f050aff49180c9834add195d&language=ko-KR&page=1`
+          )
+        ).json();
+        setMovies(json.results);
+        setLoading(false);
+      }
+      else {
+        const json = await (
+          await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=522ad530f050aff49180c9834add195d&language=en-EN&page=1`
+          )
+        ).json();
+        setMovies(json.results);
+        setLoading(false);
+      }
+    }
+  };
+
+  const onClickLogo = async() => {
+    setResult("");
+    setQuery("");
+    setBoolean(false);
+    getMovie();
   };
 
   useEffect(() => {
+    setLang(true)
     getMovie()
     setSearchKey("movie")
   }, []);
@@ -87,6 +181,11 @@ function Home() {
     />
   );
 
+  const onButtonClick = () => {
+    setLang(!lang);
+    getMovie();
+  };
+
   return ( 
     <div style={{ display: "flex", justifyContent: "center" }}>
       {loading ? (
@@ -94,7 +193,7 @@ function Home() {
       ) : (
       <div>
         <div className="Title" style={{ fontSize: "100px" }}>
-          <Link to={`/`} className="Link">
+          <Link to={`/`} className="Link" onClick={onClickLogo}>
             Today's Movies
           </Link>
         </div>
@@ -121,13 +220,23 @@ function Home() {
                 width: 200,
               }}
             />
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            {lang ? 
+              <Button type="primary" shape="circle" onClick={onButtonClick}>
+                Kor
+              </Button>
+              :
+              <Button type="primary" shape="circle" onClick={onButtonClick}>
+                Eng
+              </Button>
+              }
           </div>
         </Fragment>
           <div className="Title" style={{ display: "flex", justifyContent: "center" }}>
           {v=="1"&&query ? (
             searchKey == "movie" ? (<h1>{query} 영화 검색</h1>) : (<h1>{query} 인물 검색</h1>)
           ) : (
-            !result ? (<h1>최근 인기 영화</h1>) : (<h1>{result} 영화 검색</h1>)
+            !result ? (lang ? (<h1>최근 인기 영화</h1>) : (<h1>Popular Movies</h1>)) : (lang ? (<h1>{result} 영화 검색</h1>) : (<h1>{result} search</h1>))
           )}
           </div>
           <div className="Title">
